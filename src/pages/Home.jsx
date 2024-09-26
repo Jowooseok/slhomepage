082ecbd1cffceaa7178272e1e7480336beecab00
DESKTOP-EQSGHUM\whdnt <whdntjrsla12@naver.com>
2024-09-27 02:51:22 +0900

    여백 적용

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-// 필요한 라이브러리를 설치해야 합니다.
+// You need to install the necessary libraries.
 // npm install gsap
 
 import React, { useRef, useState, useEffect } from 'react';
@@ -6,7 +6,6 @@ import { FaPlayCircle } from 'react-icons/fa';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Videos from '../assets/Videos';
-import { Modal } from 'antd';
 import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebaseConfig';
 
@@ -15,34 +14,13 @@ gsap.registerPlugin(ScrollTrigger);
 function Home() {
   const videoRef = useRef(null);
   const overlayRef = useRef(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const [email, setEmail] = useState('');
-
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleOk = () => {
-    setIsModalOpen(false);
-    if (videoRef.current) {
-      videoRef.current.pause();
-      videoRef.current.currentTime = 0;
-    }
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-    if (videoRef.current) {
-      videoRef.current.pause();
-      videoRef.current.currentTime = 0;
-    }
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (!email) {
-      alert('이메일을 입력해주세요.');
+      alert('Please enter your email.');
       return;
     }
 
@@ -53,10 +31,10 @@ function Home() {
       });
       setEmail('');
       alert(
-        '구독해주셔서 감사합니다! 곧 SL Protocol의 업데이트를 받아보실 수 있습니다.'
+        'Thank you for subscribing! You will soon receive updates from SL Protocol.'
       );
     } catch (err) {
-      console.error('문서 추가 중 오류 발생: ', err);
+      console.error('Error adding document: ', err);
     }
   };
 
@@ -64,16 +42,17 @@ function Home() {
     const overlayElement = overlayRef.current;
 
     if (overlayElement) {
-      gsap.timeline({
-        scrollTrigger: {
-          trigger: overlayElement,
-          start: 'top top',
-          end: '+=1000', // 스크롤 거리 조정
-          scrub: true,
-          pin: true,
-          anticipatePin: 1,
-        },
-      })
+      gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: overlayElement,
+            start: 'top top',
+            end: '+=1000', // Adjusted scroll distance
+            scrub: true,
+            pin: true,
+            anticipatePin: 1,
+          },
+        })
         .to(overlayElement, { opacity: 1, duration: 0 })
         .to(
           overlayElement,
@@ -93,11 +72,11 @@ function Home() {
   return (
     <div>
       <div className="relative w-full overflow-hidden">
-        {/* 비디오 및 오버레이 컨테이너 */}
-        <div className="relative w-full h-screen md:p-8 p-4 md:pt-0 pt-0">
-          {/* 비디오와 오버레이를 담은 컨테이너 */}
+        {/* Video and Overlay Container */}
+        <div className="relative w-full h-[93vh] md:p-8 p-4 md:pt-0 pt-0">
+          {/* Container holding the video and overlay */}
           <div className="relative w-full h-full">
-            {/* 배경 비디오 */}
+            {/* Background Video */}
             <video
               ref={videoRef}
               className="absolute inset-0 w-full h-full object-cover rounded-2xl"
@@ -109,10 +88,10 @@ function Home() {
               style={{ pointerEvents: 'none' }}
             />
 
-            {/* 반투명 오버레이 */}
+            {/* Semi-transparent Overlay */}
             <div className="absolute inset-0 bg-black opacity-50 rounded-2xl"></div>
 
-            {/* 오버레이 콘텐츠 */}
+            {/* Overlay Content */}
             <div
               ref={overlayRef}
               className="absolute inset-0 flex flex-col items-center justify-center text-center z-10 p-4 md:p-8 space-y-8 text-white"
@@ -127,14 +106,14 @@ function Home() {
                 Your data saves lives
               </h1>
 
-              {/* 이메일 구독 폼 */}
+              {/* Email Subscription Form */}
               <form className="w-full max-w-md" onSubmit={handleSubmit}>
-                <p className="text-lg md:text-2xl mb-4">업데이트를 구독하세요</p>
+                <p className="text-lg md:text-2xl mb-4">Subscribe for updates</p>
                 <div className="flex items-center">
                   <input
                     className="appearance-none bg-transparent border border-white rounded-2xl w-full h-12 text-white px-4 leading-tight focus:outline-none text-sm md:text-base"
                     type="email"
-                    placeholder="이메일을 입력하세요"
+                    placeholder="Enter your email"
                     aria-label="Email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
@@ -143,32 +122,34 @@ function Home() {
                     className="flex-shrink-0 bg-white h-12 hover:bg-gray-200 text-black w-28 font-semibold rounded-2xl ml-2 text-sm md:text-base"
                     type="submit"
                   >
-                    구독하기
+                    Subscribe
                   </button>
                 </div>
               </form>
 
-              {/* 하단의 'Play Video' 버튼 */}
+              {/* 'Play Video' Button */}
               <div className="pt-8">
-                <button
-                  onClick={showModal}
+                <a
+                  href="https://drive.google.com/file/d/1FQozCroeIAvCReiDOJkTK6bgh76t4nJ5/view"
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="flex items-center text-base md:text-xl focus:outline-none"
                 >
                   <FaPlayCircle className="mr-2" />
-                  비디오 재생
-                </button>
+                  Play Video
+                </a>
               </div>
             </div>
           </div>
         </div>
       </div>
 
-      {/* 스크롤 테스트를 위한 추가 섹션들 */}
+      {/* Additional Sections for Scroll Testing */}
       <div className="bg-gray-100 py-20">
         <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8">섹션 1</h2>
+          <h2 className="text-3xl font-bold mb-8">Section 1</h2>
           <p className="mb-4">
-            이곳은 스크롤을 테스트하기 위한 첫 번째 섹션입니다.
+            This is the first section to test scrolling.
           </p>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
@@ -179,9 +160,9 @@ function Home() {
 
       <div className="bg-white py-20">
         <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8">섹션 2</h2>
+          <h2 className="text-3xl font-bold mb-8">Section 2</h2>
           <p className="mb-4">
-            이곳은 스크롤을 테스트하기 위한 두 번째 섹션입니다.
+            This is the second section to test scrolling.
           </p>
           <p>
             Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras
@@ -192,9 +173,9 @@ function Home() {
 
       <div className="bg-gray-100 py-20">
         <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8">섹션 3</h2>
+          <h2 className="text-3xl font-bold mb-8">Section 3</h2>
           <p className="mb-4">
-            이곳은 스크롤을 테스트하기 위한 세 번째 섹션입니다.
+            This is the third section to test scrolling.
           </p>
           <p>
             Donec ullamcorper, felis non sodales commodo, lectus velit ultrices
@@ -202,24 +183,6 @@ function Home() {
           </p>
         </div>
       </div>
-
-      {/* 비디오 모달 */}
-      <Modal
-        open={isModalOpen}
-        onOk={handleOk}
-        onCancel={handleCancel}
-        footer={null}
-        width="80%"
-        centered
-        bodyStyle={{ padding: 0 }}
-      >
-        <video
-          className="w-full h-full"
-          src={Videos.CRXVedio}
-          controls
-          autoPlay
-        />
-      </Modal>
     </div>
   );
 }
